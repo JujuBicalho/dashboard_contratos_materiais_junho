0db4e88d794f7abe8f85d2da007c6f94329658c7
Juliana <julianabbicalho@gmail.com>
2024-06-13 09:11:17 -0300

    Atualização 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -136,14 +136,15 @@ fig_consumo.update_xaxes(
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = dbc.Container([
-    html.Div([
+       html.Div([
         html.H1("Análise Descritiva Contratos de Materiais - Junho 2024", style={
             'textAlign': 'center', 
             'color': '#005a8d', 
             'backgroundColor': '#F0F8FF', 
             'padding': '20px', 
             'border-radius': '10px',
-            'width': '100%'
+            'width': '100%',
+            'fontSize': '24px'  
         }),
     ], style={'marginBottom': '40px', 'width': '100%'}),
     
